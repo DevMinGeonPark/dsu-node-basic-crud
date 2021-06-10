1bbae38c11171d4ebc457cc19ee2c1a06dcf9710
MinGeonPark <devmin7985@gmail.com>
2021-06-11 06:23:42 +0900

    code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-require("sanitize");
+
 const express = require('express');
 const fs = require('fs');
 const ejs = require('ejs');
@@ -7,90 +7,98 @@ const mysql = require('mysql');
 
 // dotenv package를 통해 .env에 값을 저장하고 활용
 const client = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB
 })
 
 const app = express();
 
 app.use(express.urlencoded({
-  extended: false
+    extended: false
 }))
 
-app.listen(8080, function () {
-  console.log('Server is running at : http://127.0.0.1:8080');
+//8080 port server
+app.listen(8080, () => {
+    console.log('Server is running at : http://127.0.0.1:8080');
 })
 
-app.get('/', function (req, res) {
-  fs.readFile('index.ejs', 'utf8', function (err, data) {
-    client.query('select * from Users', function (err, results) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.send(ejs.render(data, {
-          data: results
-        }))
-      }
+//get index.ejs
+app.get('/', (req, res) => {
+    fs.readFile('index.ejs', 'utf8', (err, data) => {
+        client.query('select * from Users', (err, results) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.send(ejs.render(data, {
+                    data: results
+                }))
+            }
+        })
     })
-  })
 })
 
-app.get('/delete/:id', function (req, res) {
-  client.query('delete from Users where id=?', [req.params.id], function () {
-    res.redirect('/')
-  })
-})
-
-app.get('/src/insert', function (req, res) {
-  fs.readFile('src/insert.html', 'utf8', function (err, data) {
-    res.send(data)
-  })
-})
-
-app.post('/src/insert', function (req, res) {
-  const body = req.body;
-
-  client.query('insert into Users (Name, OS, Version) values (?, ?, ?);', [
-    body.Name,
-    body.OS,
-    body.Version
-  ], function () {
-    res.redirect('/')
-  })
-})
-app.get('/src/edit/:id', function (req, res) {
-  fs.readFile('src/edit.ejs', 'utf8', function (err, data) {
-    client.query('select * from Users where id = ?', [req.params.id], function (err, result) {
-      res.send(ejs.render(data, {
-        data: result[0]
-      }))
+//insert : C (create)
+app.get('/src/insert', (req, res) => {
+    fs.readFile('src/insert.html', 'utf8', (err, data) => {
+        res.send(data)
     })
-  })
 })
 
-app.post('/src/edit/:id', function (req, res) {
-  const body = req.body;
+app.post('/src/insert', (req, res) => {
+    const body = req.body;
 
-  client.query('update Users Set Name=?, OS=?, Version=? where id=? ', [
-    body.Name, body.OS, body.Version, req.params.id
-  ], function () {
-    res.redirect('/')
-  })
-})
-
-app.post('/serach-query', function (req, res) {
-    var word = `select * from Users where Name like '%${req.body.Name}%'`
-    fs.readFile('src/search.ejs', 'utf8', function (err, data) {
-      client.query(word, function (err, results) {
-        if (err) {
-          res.send(err)
-        } else {
-          res.send(ejs.render(data, {
-            data: results
-          }))
-        }
-      })
+    client.query('insert into Users (Name, OS, Version) values (?, ?, ?);', [
+        body.Name,
+        body.OS,
+        body.Version
+    ], () => {
+        res.redirect('/')
     })
-  })
+})
+
+//Search : R (Reading)
+app.post('/search-query', (req, res) => {
+    var word = `select * from Users where Name like '%${req.body.Name}%'`;
+    fs.readFile('src/search.ejs', 'utf8', (err, data) => {
+        client.query(word, function (err, results) {
+            if (err) {
+                res.send(err)
+            } else {
+                res.send(ejs.render(data, {
+                    data: results
+                })
+                )
+            }
+        })
+    })
+})
+
+//Edit : U (Update)
+app.get('/src/edit/:id', (req, res) => {
+    fs.readFile('src/edit.ejs', 'utf8', (err, data) => {
+        client.query('select * from Users where id = ?', [req.params.id], (err, result) => {
+            res.send(ejs.render(data, {
+                data: result[0]
+            }))
+        })
+    })
+})
+
+app.post('/src/edit/:id', (req, res) => {
+    const body = req.body;
+
+    client.query('update Users Set Name=?, OS=?, Version=? where id=? ', [
+        body.Name, body.OS, body.Version, req.params.id
+    ], () => {
+        res.redirect('/')
+    })
+})
+
+//delete : D (Delete)
+app.get('/delete/:id', (req, res) => {
+    client.query('delete from Users where id=?', [req.params.id], () => {
+        res.redirect('/')
+    })
+})
